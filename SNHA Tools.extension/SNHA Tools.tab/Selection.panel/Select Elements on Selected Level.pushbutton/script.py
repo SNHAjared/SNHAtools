c8244874a3cb5dc30a0460c18345ba0e799a2d0a
SNHAjared <92219968+SNHAjared@users.noreply.github.com>
2024-10-30 10:00:08 -0500

    fixed Category name; error handling;

--- SNHA Tools.extension/SNHA Tools.tab/Selection.panel/Select Elements on Selected Level.pushbutton/script.py
+++ SNHA Tools.extension/SNHA Tools.tab/Selection.panel/Select Elements on Selected Level.pushbutton/script.py
@@ -1,4 +1,4 @@
-#   "Select Elements on Selected Level"
+#   "Select Elements on Selected"
 #   Copyright 2024 Jared M. Holloway
 #   License: MIT
 #   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -13,7 +13,10 @@ doc = HOST_APP.doc
 uidoc = HOST_APP.uidoc
 selection = revit.get_selection()
 
-if selection[0].Category.Name == "Level":
-    selected_level = selection[0]
-    
-uidoc.Selection.SetElementIds(FilteredElementCollector(doc).WherePasses(ElementLevelFilter(selected_level.Id)).ToElementIds())
+try:
+    assert selection[0].Category.Name == "Levels"
+except AssertionError:
+    raise Exception("Please select one Level.")
+else:
+    selected_level = selection[0] 
+    uidoc.Selection.SetElementIds(FilteredElementCollector(doc).WherePasses(ElementLevelFilter(selected_level.Id)).ToElementIds())
